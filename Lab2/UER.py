789ad9e7ae1b362ce429a6dd30f389543d361cf5
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-03-16 10:06:09 +0100

    UER works

--- Lab2/UER.py
+++ Lab2/UER.py
@@ -1,24 +1,26 @@
 from random import random
 
 
-def gen_direct_tree(nodes, threshold):
+def gen_direct_tree(n, p):
     data = dict()
-    for u in range(0, nodes):
+    for u in range(0, n):
         data[u] = set()
-        for v in range(0, nodes):
+        for v in range(0, n):
             a = random()
-            if a < threshold:
+            if a < p:
                 data[u].add(v)
     return data
 
 
-def gen_indirect_tree(nodes, threshold):
+def gen_indirect_tree(n, p):
     data = dict()
-    for u in range(0, nodes):
+    for u in range(0, n):
         data[u] = set()
-        for v in range(0, nodes):
+        for v in range(0, n):
             a = random()
-            if a < threshold:
+            if a < p:
+                data[v] = set()
+
                 data[u].add(v)
                 data[v].add(u)
     return data
---
+++ Lab2/secondLab.py
@@ -0,0 +1,14 @@
+
+
+import matplotlib.pyplot as plt
+
+from Lab2.UER import gen_indirect_tree
+from Lab2.UPA import *
+
+#1
+# Dopo aver calcolato la resilienza dei tre grafi, mostrate il risultato in un grafico con scala lineare (non log/log) che combini le tre curve ottenute. Usate un grafico a punti oppure a linea per ognuna delle curve. L'asse orizzontale del grafico deve corrispondere al numero di nodi disattivati dall'attacco (che variano da 0 a n), mentre l'asse verticale alla dimensione della componente connessa più grande rimasta dopo aver rimosso un certo numero di nodi. Aggiungete una legenda al grafico che permetta di distinguere le tre curve e che specifici i valori di p e m utilizzati. Allegate il file con la figura nell'apposito spazio.
+
+
+uer = gen_indirect_tree(1476, 0.00145290)
+
+print()
