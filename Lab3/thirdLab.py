2a797b89162f202fdce675ff1b9120f6391fcce1
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-03-26 12:16:13 +0200

    Lab3 init

---
+++ Lab3/thirdLab.py
@@ -0,0 +1,11 @@
+# 1
+# Usate l'algoritmo CCRP per trovare un piano di evacuazione nel grafo della Città di San Francisco. I nodi
+# sorgente del piano sono i tre ingressi autostradali della città:
+#
+# nodo 3718987342 (Golden Gate Bridge) nodo 915248218 (Oakland Bay Bridge) nodo 65286004 (James Lick Freeway) I nodi
+# destinazione corrispondono a sei ospedali cittadini: nodo 261510687 (San Francisco General Hospital) nodo
+# 3522821903 (UC Medical Center) nodo 65319958 (Saint Francis Memorial Hospital) nodo 65325408 (Saint Mary's Medical
+# Center) nodo 65295403 (CPMP California Campus) nodo 258913493 (Kaiser-Permanente Medical Center) Mostrate
+# l'andamento dell'algoritmo in un grafico a linee dove l'asse x corrisponde alle capacità e l'asse y al tempo di
+# percorrenza. Il grafico deve mostrare come cresce la capacità ed il tempo di percorrenza al crescere del numero di
+# percorsi inseriti nel piano dall'algoritmo.
