e686fb339850bcd397754c2ddb35d30119f5c143
Filippo Berto <berto.f@protonmail.com>
2018-04-24 09:44:57 +0200

    Continuo libreria

--- LibGraph/_test/PIMapIndirectGraphTest.py
+++ LibGraph/_test/PIMapIndirectGraphTest.py
@@ -19,3 +19,13 @@ class TestPIMapIndirectGraph(unittest.TestCase):
         graph.remove_arch((5, 7), (6, 2))
 
         self.assertEqual({(frozenset({(6, 2), (4, 8)}), 3), (frozenset({(8, 3), (4, 1)}), 7)}, graph.get_arch_list())
+        self.assertEqual({(8, 3), (4, 8), (5, 7), (6, 2), (4, 1)}, graph.get_node_list())
+
+        print("skfm")
+        print(graph.get_arch_list())
+        print(graph.get_node_list())
+
+        graph.remove_node((6, 2))
+
+        print(graph.get_arch_list())
+        print(graph.get_node_list())
