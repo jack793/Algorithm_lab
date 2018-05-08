9d2d6edf333b22eac7e97ad98d31d8c072edc352
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-05-08 16:08:14 +0200

    cheapest_insertion seems works (pd finally)

--- Lab4/algorithms.py
+++ Lab4/algorithms.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 
 INFINITY = float('inf')
@@ -35,6 +34,20 @@ def held_karp(graph, v, s):
         return min_dist
 
 
+def get_cycle_cost(graph, cycle):
+    adj_matrix = graph.get_adj_matrix()
+    cost = 0
+    length = len(cycle)
+    for i in range(length):
+        j = i + 1
+        if j == length:
+            # out of bounds
+            j = 0
+        cost += adj_matrix[cycle[i]][cycle[j]]
+
+    return cost
+
+
 #       METHOD OF CHEAPEST INSERTION
 # 1. Choose an arbitrary node i as a starting node.
 # 2. Find a node j closest to i. Form a subtour T = i-j-i.
@@ -65,44 +78,78 @@ def cheapest_insertion(graph):
 
     not_extracted_nodes.remove(c[1])
 
-    def get_new_k():
-        """
-        Find new k value to insert in the cycle
-        :return: (i,k)
-            i - left node index in the list
-            k - new node to insert
-        """
+    # def get_new_k():
+    #     """
+    #     Find new k value to insert in the cycle
+    #     :return: (i,k)
+    #         i - left node index in the list
+    #         k - new node to insert
+    #     """
+    #
+    #     res = (-2, -1, math.inf)
+    #     # for node in range(len(c)):
+    #     #     i, j = (c[node], c[(node + 1) % len(c)])
+    #     #     indexes = [k for k in extraction_list]
+    #     #     new_res = node, min(indexes, key=lambda k: adj_matrix[i, k] + adj_matrix[k, j] - adj_matrix[i, j])
+    #     #     res = min(res, new_res, key=lambda v: v[1])
+    #     #     print(adj_matrix[c[res[0]], res[1]] + adj_matrix[c[(res[0] + 1) % len(c)], res[1]])
+    #
+    #     indexes = [
+    #         (i, k, j)
+    #         for i, j in map(lambda v: (v, (v + 1) % len(c)), range(len(c)))
+    #         for k in not_extracted_nodes]
+    #     i, k, j = min(indexes,
+    #                   key=lambda tup: adj_matrix[c[tup[0]], tup[1]] + adj_matrix[tup[1], c[tup[2]]] - adj_matrix[
+    #                       c[tup[0]], c[tup[2]]])
+    #
+    #     dist = adj_matrix[c[i], k] + adj_matrix[k, c[j]] - adj_matrix[c[i], c[j]]
+    #
+    #     res = min(res, (i, k, dist), key=lambda v: v[2])
+    #
+    #     if res is (-1, 0, math.inf):
+    #         raise Exception("No node found in the extraction list")
+    #
+    #     return res
 
-        res = (-2, -1, math.inf)
-        # for node in range(len(c)):
-        #     i, j = (c[node], c[(node + 1) % len(c)])
-        #     indexes = [k for k in extraction_list]
-        #     new_res = node, min(indexes, key=lambda k: adj_matrix[i, k] + adj_matrix[k, j] - adj_matrix[i, j])
-        #     res = min(res, new_res, key=lambda v: v[1])
-        #     print(adj_matrix[c[res[0]], res[1]] + adj_matrix[c[(res[0] + 1) % len(c)], res[1]])
+    def cheapest_selection(not_extracted, c):
+        min_value = INFINITY
+        edges = None
+        next_node = None
+        for i, node in enumerate(c):
+            # for all k (nodes) not yet extracted we search for a k and a edge(_,_) that min the cycle cost
+            for k in not_extracted:
+                print(k)
+                j = i + 1
+                if j == len(c):
+                    # if out of bound w/ y, finished the hcycle
+                    j = 0
 
-        indexes = [
-            (i, k, j)
-            for i, j in map(lambda v: (v, (v + 1) % len(c)), range(len(c)))
-            for k in not_extracted_nodes]
-        i, k, j = min(indexes,
-                      key=lambda tup: adj_matrix[c[tup[0]], tup[1]] + adj_matrix[tup[1], c[tup[2]]] - adj_matrix[
-                          c[tup[0]], c[tup[2]]])
+                if adj_matrix[c[i]][k] + adj_matrix[c[j]][k] - adj_matrix[c[i]][c[j]] < min_value:
+                    min_value = adj_matrix[c[i]][k] + adj_matrix[c[j]][k] - adj_matrix[c[i]][c[j]]
+                    edges = (i, j)
+                    next_node = k
 
-        dist = adj_matrix[c[i], k] + adj_matrix[k, c[j]] - adj_matrix[c[i], c[j]]
+        return edges, next_node
 
-        res = min(res, (i, k, dist), key=lambda v: v[2])
+    while len(not_extracted_nodes) is not 0:
+        # precedent_node_index, new_node, new_dist = get_new_k()
+        # not_extracted_nodes.remove(new_node)
+        # c.insert(precedent_node_index + 1, new_node)
 
-        if res is (-1, 0, math.inf):
-            raise Exception("No node found in the extraction list")
+        edges, k = cheapest_selection(not_extracted_nodes, c)
 
-        return res
+        not_extracted_nodes.remove(k)
 
-    while not_extracted_nodes:
-        precedent_node_index, new_node, new_dist = get_new_k()
-        not_extracted_nodes.remove(new_node)
-        c.insert(precedent_node_index + 1, new_node)
+        # connect last node w/ the 1st
+        if edges[1] == 0:
+            c.append(k)
+        else:
+            c.insert(edges[1], k)
 
-    for v in list(map(lambda v: (c[v], c[(v + 1) % len(c)], adj_matrix[v, (v + 1) % len(c)]), range(len(c)))):
-        print(v)
-    return sum(map(lambda v: adj_matrix[v, (v + 1) % len(c)], range(len(c))))
+    return get_cycle_cost(graph, c)
+
+    # for v in list(
+    #         map(lambda v: (c[v], c[(v + 1) % len(c)], adj_matrix[v, (v + 1) % len(c)]),
+    #             range(len(c)))):
+    #     print(v)
+    # return sum(map(lambda v: adj_matrix[v, (v + 1) % len(c)], range(len(c))))
