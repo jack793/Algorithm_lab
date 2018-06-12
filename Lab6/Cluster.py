2cd35ce22593d0ef7be1025b5da5c2b4e00a3fe9
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-06-12 17:19:24 +0200

    progress with the cluster class..

--- Lab6/Cluster.py
+++ Lab6/Cluster.py
@@ -2,26 +2,35 @@ class Cluster(object):
     """
     Management class for cluster
     """
+
     def __init__(self, elements: set):
         """
         :param: elements: set of points to insert in the cluster
         """
+        assert len(elements) > 0
         self._elements = elements  # array of tuple of points (xi,yi)
-        self._centroid = self.calculate_centroid()
+        self._centroid = self.calculate_centroid()  # function that calculated the centroid
 
     def calculate_centroid(self):
         """
         function that returns the cluster's centroid
         """
 
+        sum_x = 0
+        sum_y = 0
+
+        for i in self._elements:
+            sum_x += i.x
+            sum_y += i.y
+
         return self._centroid
 
-    def get_points(self):
+    def get_elements(self):
         return self._elements
 
     def get_centroid(self):
         return self._centroid
 
-    def union(self, cluster: Cluster) -> None:
-        self._elements = self._elements.union(cluster.get_points())
+    def union(self, cluster) -> None:
+        self._elements = self._elements.union(cluster.get_elements())
         self._centroid = self.calculate_centroid()
