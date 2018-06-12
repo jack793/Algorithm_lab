9db7fb7188d2a4481784cbf6cf91a69afb010c18
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-06-12 17:34:23 +0200

    fix 2

--- Lab6/Algorithms.py
+++ Lab6/Algorithms.py
@@ -1,14 +1,14 @@
-
 from Lab6.Cluster import *
 
-def HierarchicalClustering(p, k):
+
+def hierarchical_clustering(p, k):
     insieme = set()
     for point in p:
-        c = Cluster([point])
+        c = Cluster({point})
         insieme.add(c)
 
     while len(insieme) > k:
-        i, j = #invoca fastclosestpair
+        i, j =  # fastclosest_pair
         i = i.union(j)
         insieme.remove(j)
 
