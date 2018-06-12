e1d901e13066cf11333db7c8edcc072619451fcf
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-06-12 16:50:34 +0200

    progress with the cluster class..

--- Lab6/cluster.py
+++ Lab6/cluster.py
@@ -1,9 +1,19 @@
+"""
+Management class for cluster
+"""
 
 
 class Cluster:
-    def __init__(self, elem):
+    def __init__(self, elements):
         """
-        :param: elements:
+        :param: elements: array of elements need for initialization
         """
-        self._elements = elem  # array of tuple of points (xi,yi)
+        self._elements = elements  # array of tuple of points (xi,yi)
         self._centroid = self.calculate_centroid()
+
+    def calculate_centroid(self):
+        """
+        function that returns the cluster's centroid
+        """
+
+        return self._centroid
