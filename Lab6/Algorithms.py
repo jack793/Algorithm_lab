ea124486ec82c88473f662af753307bcaed46a28
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2018-06-13 11:19:54 +0200

    init fast_closest_pair and concluded slow_closest_pair

--- Lab6/Algorithms.py
+++ Lab6/Algorithms.py
@@ -1,3 +1,5 @@
+from math import inf, floor
+
 from Lab6.Cluster import *
 from Lab6.Point import *
 
@@ -32,6 +34,26 @@ def k_means_clustering(p: {Point}, k: int, q: int):
         return c
 
     for iteration in range(q):
-        #TODO continue
+        # TODO continue
         pass
 
+
+def slow_closest_pair(p):
+    min_distance = inf
+    nearest_points = None  # tuple (i,j) point coordinates
+
+    for pu, i in enumerate(p):
+        for pv, j in enumerate(p):
+            if pv > pu:
+                distance = Point.distance(i.x, j.x)
+                if distance < min_distance:  # smaller distance between pairs of points (i,j)
+                    min_distance = distance
+                    nearest_points = (i, j)
+
+    return min_distance, nearest_points.x, nearest_points.y
+
+
+def fast_closest_pair(p, s):
+    n = len(p)
+    if n <= 3:
+        return slow_closest_pair(p)
