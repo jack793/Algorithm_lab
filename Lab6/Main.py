e3adb8c5e3dddc2be2f641c7cfd52c5e60072f8b
Filippo Berto <berto.f@protonmail.com>
2018-06-13 18:49:18 +0200

    Test plot on image

--- Lab6/Main.py
+++ Lab6/Main.py
@@ -11,4 +11,24 @@ with open("unifiedCancerData/unifiedCancerData_111.csv", "r") as csv_file:
         points.add(County(int(row[0]), float(row[1]), float(row[2]), int(row[3]), float(row[4])))
 
     # print(hierarchical_clustering(points, 15))
-    print(k_means_clustering(points, 15, 100))
+    # print(k_means_clustering(points, 15, 100))
+    #
+    # map_img = plt.imread("map/USA_Counties.png")
+    # im_plot = plt.imshow(map_img)
+    #
+    # xs = list()
+    # ys = list()
+    # ps = list()
+    # rs = list()
+    #
+    # for row in reader:
+    #     xs.append(float(row[1]))
+    #     ys.append(float(row[2]))
+    #     ps.append(log(float(row[3])) / 5)
+    #     rs.append(float(row[4]))
+    #
+    # plt.scatter(x=xs, y=ys, s=ps, c=linspace(0, 1000, 3108))
+    # plt.show()
+    #
+    # # print(hierarchical_clustering(points, 15))
+    # # print(k_means_clustering(points, 15, 100))
